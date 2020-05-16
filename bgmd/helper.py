399700a23dcc04dba8f889da50e7db3e40cdb58b
no1xsyzy <mf20070535@126.com>
2020-05-17 00:48:09 +0800

    feat(bgmd.helper): new helper function for multi-page spider

--- bgmd/helper.py
+++ bgmd/helper.py
@@ -9,3 +9,17 @@ def crop_mono(html):
     if (pin := column_a.find('div', {'class': 'page_inner'})) is not None:
         all_pages = max(int(a['href'].rsplit("=", 1)[1]) for a in pin.find_all('a', {'class': 'p'}))
     return [link.find('a', {'class', 'l'})['href'] for link in cc], all_pages
+
+
+def multi_page(getter, base_url, selector):
+    result = []
+    soup = BeautifulSoup(getter(base_url).content, 'html.parser')
+    result.extend(soup.select(selector))
+    if (pin := soup.select_one("div.page_inner")) is not None:
+        all_pages = max(int(a['href'].rsplit("=", 1)[1]) for a in pin.find_all('a', {'class': 'p'}))
+    else:
+        all_pages = 1
+    for page in range(2, all_pages+1):
+        soup = BeautifulSoup(getter(base_url, params=(('page', page), )).content, 'html.parser')
+        result.extend(soup.select(selector))
+    return result
