829dbfc0a7cea142ec4e093cf5254f573e976be9
no1xsyzy <mf20070535@126.com>
2020-05-17 00:49:10 +0800

    feat(bgmd.model): Enhance model to more details

--- bgmd/model.py
+++ bgmd/model.py
@@ -1,9 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import lru_cache
 from typing import *
 import requests
 
-__all__ = ['User', 'Mono', 'Character', 'Person', 'Login']
+__all__ = ['User', 'Login',
+           'MonoBase', 'Images', 'Mono',
+           'Character', 'Person']
 
 
 @dataclass(frozen=True)
@@ -14,9 +16,38 @@ class User:
     nickname: str
 
 
+@dataclass()
+class Images:
+    large: Optional[str] = field(default=None)
+    medium: Optional[str] = field(default=None)
+    small: Optional[str] = field(default=None)
+    grid: Optional[str] = field(default=None)
+
+
 @dataclass(frozen=True)
-class Mono:
+class MonoBase:
     id: int
+    url: Optional[str] = field(default=None, compare=False)
+    name: Optional[str] = field(default=None, compare=False)
+    images: Optional[Images] = field(default=None, compare=False)
+
+    def is_detail(self):
+        return (self.url is not None and
+                self.name is not None and
+                self.images is not None)
+
+
+@dataclass(frozen=True)
+class Mono(MonoBase):
+    name_cn: Optional[str] = field(default=None, compare=False)
+    comment: Optional[int] = field(default=None, compare=False)
+    collects: Optional[int] = field(default=None, compare=False)
+
+    def is_detail(self):
+        return (super().is_detail() and
+                self.name_cn is not None and
+                self.comment is not None and
+                self.collects is not None)
 
 
 @dataclass(frozen=True)
