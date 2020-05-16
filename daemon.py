554c67da2e244dbf9e0eb2692d9e028f944307cc
no1xsyzy <mf20070535@126.com>
2020-05-16 18:42:22 +0800

    fix(daemon): Correct file name

--- daemon.py
+++ daemon.py
@@ -37,7 +37,7 @@ class Daemon:
             self.error_time.append(now)
             while self.error_time and now - self.error_time[0] < timedelta(minutes=1):
                 self.error_time.pop(0)
-            with open(f"exception@{now.isoformat()}.log", mode='w') as fp:
+            with open(f"exception@{now.isoformat().replace(':', '.')}.log", mode='w') as fp:
                 import sys
                 traceback.print_exc(file=fp)
                 if isinstance(e, json.decoder.JSONDecodeError):
