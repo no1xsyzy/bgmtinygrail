ab03ad0f32d51d45c1be0f5d6ae19511d7863bca
no1xsyzy <mf20070535@126.com>
2020-05-17 00:51:51 +0800

    feat(bunchbgmchara): show how character_detail works

---
+++ bunchbgmchara.py
@@ -0,0 +1,6 @@
+from bgmd.api import character_detail
+
+if __name__ == '__main__':
+    for cid in range(14805, 14817):
+        detail = character_detail(cid)
+        print(f"{detail.id}, {detail.name}, {detail.name_cn}")
