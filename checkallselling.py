2cceef1163f1be317868a356bd29b6beaea8a37e
no1xsyzy <mf20070535@126.com>
2020-05-19 01:32:12 +0800

    fix(check all selling): modularize it

--- checkallselling.py
+++ checkallselling.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
-from bgmd.api import *
-from accounts import bgm_xsb_player, tg_xsb_player
-from tinygrail.api import *
-import logging
 import logging.config
+
+from accounts import bgm_xsb_player, tg_xsb_player
+from bgmd.api import *
+from bgmd.model import Login
+from tinygrail.api import *
+from tinygrail.model import Player
 
 logger = logging.getLogger('check_all_selling')
 
-logging.config.fileConfig('logging.conf')
 
-asks = {c.character_id for c in all_asks(tg_xsb_player)}
+def check_all_selling(player: Player, login: Login, sets=True):
+    asks = {c.character_id for c in all_asks(player)}
 
-favorite_characters = {c.id for c in user_mono(user_info(username='525688'), 'character')}
+    favorite_characters = {c.id for c in user_mono(login.user, 'character')}
 
-for i in asks - favorite_characters:
-    print(f"+ https://bgm.tv/character/{i}")
-    collect_mono(bgm_xsb_player, i)
+    for i in asks - favorite_characters:
+        print(f"+ https://bgm.tv/character/{i}")
+        if sets:
+            collect_mono(login, i)
 
-for i in favorite_characters - asks:
-    print(f"- https://bgm.tv/character/{i}")
-    erase_collect_mono(bgm_xsb_player, i)
+    for i in favorite_characters - asks:
+        print(f"- https://bgm.tv/character/{i}")
+        if sets:
+            erase_collect_mono(login, i)
+
+
+if __name__ == "__main__":
+    logging.config.fileConfig('logging.conf')
+    check_all_selling(tg_xsb_player, bgm_xsb_player, True)
