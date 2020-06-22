c8dc04f8c31559ac56648b605a7249e2285e3dab
no1xsyzy <mf20070535@126.com>
2020-06-23 01:27:14 +0800

    fix(checkumb): missing import and other 3 problems
    rearrange import order
    wrap in main()
    better exception processing

--- checkumb.py
+++ checkumb.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
-import requests
-import re
-from bs4 import BeautifulSoup
 from typing import *
-from tinygrail.api import batch_character_info
+
+from dacite.exceptions import MissingValueError
+
+from accounts import no1xsyzy
 from bgmd.api import person_work_voice_character
 from bgmd.model import *
-from accounts import no1xsyzy
 from data import *
+from tinygrail.api import batch_character_info, user_temples
 
 
-aoi_chan = Person(5076)
+def main():
+    try:
+        aoi_chan = Person(5076)
+        umb_characters: Set[int] = {c.id for c in person_work_voice_character(aoi_chan)}
+        temples = user_temples(no1xsyzy)
+        temple_list = [t.character_id for t in temples if t.assets >= 2500]
+        checklist: Set[int] = umb_characters - {cid for cid in (follow_list + temple_list + umb_ignore_list)}
+        print("\n".join(f"https://bgm.tv/character/{chara.character_id}"
+                        for chara in batch_character_info(no1xsyzy, checklist)))
+    except MissingValueError:
+        import inspect
+        from pprint import pprint
+        original_data = inspect.trace()[-1][0].f_locals['data']
+        pprint(original_data)
 
-umb_characters: Set[int] = {c.id for c in person_work_voice_character(aoi_chan)}
 
-temples = user_temples(no1xsyzy)
-temple_list = [t.character_id for t in temples if t.assets >= 2500]
-
-checklist: Set[int] = umb_characters - {cid for cid in (follow_list + umb_temple_list + umb_ignore_list)}
-
-print("\n".join(f"https://bgm.tv/character/{chara.character_id}"
-                for chara in batch_character_info(no1xsyzy, checklist)))
+if __name__ == '__main__':
+    main()
