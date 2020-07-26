af4683e37f77bb63818ff9773f3682aab9a7c722
no1xsyzy <mf20070535@126.com>
2020-07-27 00:35:47 +0800

    refactor(tinygrail.api): use `json=` in request

--- tinygrail/api.py
+++ tinygrail/api.py
@@ -1,4 +1,3 @@
-import json
 import logging
 
 from .model import *
@@ -14,8 +13,7 @@ def batch_character_info(player, lst, splits=50) -> List[Union[TCharacter, TICO]
     while lst:
         head50 = lst[:splits]
         lst = lst[splits:]
-        data = json.dumps(head50)
-        response = player.session.post('https://tinygrail.com/api/chara/list', data=data, timeout=REQUEST_TIMEOUT)
+        response = player.session.post('https://tinygrail.com/api/chara/list', json=head50, timeout=REQUEST_TIMEOUT)
         jso = snaky(response.json())
         obj = RCharacterList(**jso)
         ans.extend(obj.value)
@@ -104,7 +102,7 @@ def create_bid(player: Player, cid: int, bid: TBid):
     url = f"https://tinygrail.com/api/chara/bid/{cid}/{bid.price}/{bid.amount}"
     if bid.type == 1:
         url += "/true"
-    response = player.session.post(url, data="null", timeout=REQUEST_TIMEOUT)
+    response = player.session.post(url, json=None, timeout=REQUEST_TIMEOUT)
     jso = response.json()
     logger.debug(jso)
     return jso
@@ -114,7 +112,7 @@ def create_ask(player: Player, cid: int, ask: TAsk):
     url = f"https://tinygrail.com/api/chara/ask/{cid}/{ask.price}/{ask.amount}"
     if ask.type == 1:
         url += "/true"
-    response = player.session.post(url, data="null", timeout=REQUEST_TIMEOUT)
+    response = player.session.post(url, json=None, timeout=REQUEST_TIMEOUT)
     jso = response.json()
     logger.debug(jso)
     return jso
@@ -123,7 +121,7 @@ def create_ask(player: Player, cid: int, ask: TAsk):
 def cancel_bid(player: Player, bid: TBid):
     assert bid.id is not None, ValueError
     url = f"https://tinygrail.com/api/chara/bid/cancel/{bid.id}"
-    response = player.session.post(url, data="null", timeout=REQUEST_TIMEOUT)
+    response = player.session.post(url, json=None, timeout=REQUEST_TIMEOUT)
     jso = response.json()
     logger.debug(jso)
     return jso
@@ -132,7 +130,7 @@ def cancel_bid(player: Player, bid: TBid):
 def cancel_ask(player: Player, ask: TAsk):
     assert ask.id is not None, ValueError
     url = f"https://tinygrail.com/api/chara/ask/cancel/{ask.id}"
-    response = player.session.post(url, data="null", timeout=REQUEST_TIMEOUT)
+    response = player.session.post(url, json=None, timeout=REQUEST_TIMEOUT)
     jso = response.json()
     logger.debug(jso)
     return jso
