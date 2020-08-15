54480baa006455147e8d908c17feb1b0538564ea
no1xsyzy <mf20070535@126.com>
2020-08-15 18:31:37 +0800

    new(cli): extract cli

---
+++ cli/__init__.py
@@ -0,0 +1,15 @@
+import click
+
+from .accounts import accounts
+from .check_auction_price_value import check_auction_price_value
+from .magic import magic
+
+
+@click.group()
+def entry_point():
+    pass
+
+
+assert isinstance(entry_point, click.Group)
+
+entry_point.add_command(accounts)
