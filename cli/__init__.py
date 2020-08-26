c337b0a518bc464d1a9472017c60281e98b204fa
no1xsyzy <mf20070535@126.com>
2020-08-27 00:56:11 +0800

    new(cli.check_all_selling): cannot replace since original also in use

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,6 +3,7 @@ import logging.config
 import click
 
 from .accounts import accounts
+from .check_all_selling import cmd_check_all_selling
 from .check_auction_price_value import check_auction_price_value
 from .check_cv import check_cv
 from .magic import magic
@@ -17,6 +18,7 @@ def entry_point(log_conf):
 assert isinstance(entry_point, click.Group)
 
 entry_point.add_command(accounts)
+entry_point.add_command(cmd_check_all_selling)
 entry_point.add_command(check_auction_price_value)
 entry_point.add_command(check_cv)
 entry_point.add_command(magic)
