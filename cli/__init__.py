17359cb95889918ce4841ad891eb7c2c06020853
no1xsyzy <mf20070535@126.com>
2020-08-29 15:04:36 +0800

    refactor(model_link.sync_asks_collect): moved from check_all_selling refactor(cli.sync_asks_collect): moved from cli.check_all_selling
    DRY

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,10 +3,10 @@ import logging.config
 import click
 
 from .accounts import accounts
-from .check_all_selling import cmd_check_all_selling
 from .check_auction_price_value import check_auction_price_value
 from .check_cv import check_cv
 from .magic import magic
+from .sync_asks_collect import cmd_sync_asks_collect
 
 
 @click.group()
@@ -18,7 +18,7 @@ def entry_point(log_conf):
 assert isinstance(entry_point, click.Group)
 
 entry_point.add_command(accounts)
-entry_point.add_command(cmd_check_all_selling)
+entry_point.add_command(cmd_sync_asks_collect)
 entry_point.add_command(check_auction_price_value)
 entry_point.add_command(check_cv)
 entry_point.add_command(magic)
--- model_link/sync_asks_collect.py
+++ model_link/sync_asks_collect.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python3
-import logging.config
 
-from accounts import bgm_xsb_player, tg_xsb_player
 from bgmd.api import *
 from bgmd.model import Login
 from tinygrail.api import *
 from tinygrail.model import Player
 
-logger = logging.getLogger('check_all_selling')
 
-
-def sync_asks_collect(player: Player, login: Login, sets=True):
+def compare_asks_collect(player: Player, login: Login):
     asks = {c.character_id for c in all_asks(player)}
-
     favorite_characters = {c.id for c in user_mono(login.user, 'character')}
 
-    for i in asks - favorite_characters:
-        print(f"+ https://bgm.tv/character/{i}")
+    return asks - favorite_characters, favorite_characters - asks
+
+
+def sync_asks_collect(player: Player, login: Login, sets=True, logs=lambda x: None):
+    anf, fna = compare_asks_collect(player, login)
+
+    for i in anf:
+        logs(f"- https://bgm.tv/character/{i}")
         if sets:
             collect_mono(login, i)
 
-    for i in favorite_characters - asks:
-        print(f"- https://bgm.tv/character/{i}")
+    for i in fna:
+        logs(f"- https://bgm.tv/character/{i}")
         if sets:
             erase_collect_mono(login, i)
-
-
-if __name__ == "__main__":
-    logging.config.fileConfig('../logging.conf')
-    sync_asks_collect(tg_xsb_player, bgm_xsb_player, True)
