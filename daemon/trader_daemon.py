2df72e61d66175215e89d03f6691fefd1189e57b
inqb <inqb@protonmail.com>
2020-09-07 15:17:14 +0800

    fix(daemon.trader_daemon): use slow starter

--- daemon/trader_daemon.py
+++ daemon/trader_daemon.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging.config
+import math
 from typing import *
 
 from model_link.sync_asks_collect import sync_asks_collect
@@ -8,6 +9,7 @@ from tinygrail.api import get_history
 from tinygrail.api import scratch_bonus2, scratch_gensokyo, scratch_gensokyo_price
 from trader import *
 from ._base import Daemon
+from random import sample
 
 logger = logging.getLogger('daemon')
 
@@ -23,11 +25,15 @@ def all_bidding_ids(player):
 class TraderDaemon(Daemon):
     trader: ABCTrader
     last_history_id: int
+    urgent_chars: Set[int]
+    slow_chars: Set[int]
 
     def __init__(self, player, login, /, *args, trader_cls=GracefulTrader, **kwargs):
         super().__init__(player, login, *args, **kwargs)
         self.trader = trader_cls(player)
         self.last_history_id = 0
+        self.urgent_chars = set()
+        self.slow_chars = set()
 
     def _update_character_due_to_history(self, full_update=False) -> List[int]:
         if full_update or self.last_history_id == 0:
@@ -48,12 +54,20 @@ class TraderDaemon(Daemon):
         return sorted(update_characters)
 
     def tick(self):
-        to_update = self._update_character_due_to_history()
-        for cid in to_update:
+        self.urgent_chars.update(self._update_character_due_to_history())
+        for cid in sorted({*self.urgent_chars,
+                           *sample(self.slow_chars, k=math.ceil(len(self.slow_chars)/5))}):
             logger.info(f"on {cid}")
-            self.safe_run(self.trader.tick, cid)
+            self.safe_run(self._tick_one, cid)
             self.notify_watchdog()
         sync_asks_collect(self.player, self.login, True)
+
+    def _tick_one(self, cid):
+        self.trader.tick(cid)
+        if cid in self.urgent_chars:
+            self.urgent_chars.remove(cid)
+        if cid in self.slow_chars:
+            self.slow_chars.remove(cid)
 
     def daily(self):
         logger.info(f"daily")
@@ -84,7 +98,7 @@ class TraderDaemon(Daemon):
                 break
             for sb in scratch_result:
                 logger.debug(f"scratch_gensokyo | got #{sb.id:<5} | {sb.amount=}, {sb.sell_price=}")
-                actual_value = self.safe_run(ticker, sb.id, sb.sell_price)
+                actual_value = self.safe_run(ticker, sb.id, sb.sell_price) or 0
                 got_value += actual_value * sb.amount
             got_value = 0
             s_price = scratch_gensokyo_price(self.player)
@@ -94,12 +108,15 @@ class TraderDaemon(Daemon):
 
     def hourly(self):
         logger.info("hourly")
-        to_update = self._update_character_due_to_history(full_update=True)
-        for cid in to_update:
-            logger.info(f"on {cid}")
-            self.safe_run(self.trader.tick, cid)
-            self.notify_watchdog()
-        sync_asks_collect(self.player, self.login, True)
+        abi = set(all_bidding_ids(self.player))
+        ahi = set(all_holding_ids(self.player))
+
+        self.slow_chars.update(abi)
+        logger.debug(f"slow chars: {sorted(self.slow_chars)}")
+
+        # holding but not bidding, indicating worn out bidding
+        self.urgent_chars.update(ahi-abi)
+        logger.debug(f"urgent chars: {sorted(self.urgent_chars)}")
         return True
 
 
