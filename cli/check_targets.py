38417f058a5f60cfac44997334664d04b45e8e3f
no1xsyzy <mf20070535@126.com>
2020-09-16 00:57:31 +0800

    enhance(cli.check_targets): can use import file now

--- cli/check_targets.py
+++ cli/check_targets.py
@@ -1,4 +1,8 @@
+from itertools import chain
+from typing import List
+
 import click
+from click.utils import LazyFile
 
 from tinygrail.api import batch_character_info, get_full_holding
 from tinygrail.model import TICO
@@ -7,15 +11,29 @@ from ._helpers import parse_target
 
 
 @click.command()
-@click.option('--player', type=TG_PLAYER)
-@click.option('--target', type=str)
+@click.argument('player', type=TG_PLAYER)
+@click.argument('targets', type=str, nargs=-1)
+@click.option('-f', '--from-file', type=click.File('r', encoding='utf-8'), multiple=True, default=[])
 @click.option('--show-exceeds/--hide-exceeds')
-def check_targets(player, target, show_exceeds):
-    targets = parse_target(target.split(","))
+def check_targets(player, targets: List[str], from_file: List[LazyFile], show_exceeds):
+    def iterates():
+        for file in from_file:
+            for line in file:
+                line = line.rstrip()
+                if line and not line.startswith("--"):
+                    yield line
+        for target in targets:
+            for t in target.split(","):
+                yield t
+
+    parsed_targets = parse_target(chain(iterates()))
+    if any(i != (0, 0) for i in parsed_targets.items()):
+        print("no target specified at all")
+        raise click.exceptions.Exit(11)
     characters = get_full_holding(player)
 
     checks = []
-    for cid, (th, tt) in targets.items():
+    for cid, (th, tt) in parsed_targets.items():
         if cid in characters:
             ch, ct = characters[cid]
             if (not (ch >= th and ct >= tt)) or (show_exceeds and not (ch == th and ct == tt)):
