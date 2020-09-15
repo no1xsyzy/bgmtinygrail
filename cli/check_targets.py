a3e0b508afc953b5bd39fc036ca8220237f2aaec
no1xsyzy <mf20070535@126.com>
2020-09-16 00:00:05 +0800

    refactor(cli.check_targets): parse_target extracted

--- cli/check_targets.py
+++ cli/check_targets.py
@@ -1,31 +1,9 @@
-import re
-from typing import *
-
 import click
 
-from bgmd.api import person_work_voice_character
-from bgmd.model import Person
 from tinygrail.api import batch_character_info, get_full_holding
 from tinygrail.model import TICO
 from ._base import TG_PLAYER
-
-PARSER_RE = re.compile(r"(?:(?P<cid>\d+)|cv/(?P<cv_id>\d+)) *= *(?P<hold>\d+)?(?:/(?P<tower>\d+))?")
-
-
-def parse_target(targets: List[str]) -> Dict[int, Tuple[int, int]]:
-    result: Dict[int, Tuple[int, int]] = {}
-    for target in targets:
-        t = PARSER_RE.fullmatch(target)
-        if t is None:
-            print(f"Bad target value `{target}`. Syntax: <cid> = <hold>/<tower>, cv/<cv_id> = <hold>/<tower>")
-            raise click.exceptions.Exit(10)
-        t = t.groupdict()
-        if t['cid'] is not None:
-            result[int(t['cid'])] = int(t['hold']), int(t['tower'])
-        elif t['cv_id'] is not None:
-            for cid in person_work_voice_character(Person(id=int(t['cv_id']))):
-                result[cid.id] = int(t['hold']), int(t['tower'])
-    return result
+from ._helpers import parse_target
 
 
 @click.command()
