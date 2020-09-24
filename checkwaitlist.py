b29a484286aa7bb13b387d43e5791f9af2ad79ca
inqb <inqb@protonmail.com>
2020-09-24 12:57:09 +0800

    delete(checkwaitlist): replaced by cli.check_targets

--- checkwaitlist.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from tinygrail.api import *
-from data import *
-from accounts import *
-
-print("\n".join(f"https://bgm.tv/character/{chara.character_id}"
-                for chara in batch_character_info(tg_xsb_player, waitlist)))
