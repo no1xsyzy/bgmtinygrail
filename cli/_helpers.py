fa65a971b7c84320fc9014d892a21dea37463f8b
inqb <inqb@protonmail.com>
2020-09-24 15:11:51 +0800

    feat(cli.helpers): can suppress part now

--- cli/_helpers.py
+++ cli/_helpers.py
@@ -5,7 +5,7 @@ import click
 
 __all__ = ['parse_target']
 
-PARSER_RE = re.compile(r"(?:(?P<cid>\d+)|cv/(?P<cv_id>\d+)) *= *(?P<hold>\d+)?(?:/(?P<tower>\d+))?")
+PARSER_RE = re.compile(r"(?:(?P<cid>\d+)|cv/(?P<cv_id>\d+)) *= *(?P<hold>\d+)?(?:/(?P<tower>\d*))?")
 
 
 def parse_target(targets: Iterable[str]) -> Dict[int, Tuple[int, int]]:
@@ -17,9 +17,9 @@ def parse_target(targets: Iterable[str]) -> Dict[int, Tuple[int, int]]:
             raise click.exceptions.Exit(10)
         t = t.groupdict()
         if t['cid'] is not None:
-            result[int(t['cid'])] = int(t['hold']), int(t['tower'])
+            result[int(t['cid'])] = int(t['hold'] or 0), int(t['tower'] or 0)
         elif t['cv_id'] is not None:
             from bgmd import person_work_voice_character, Person
             for cid in person_work_voice_character(Person(id=int(t['cv_id']))):
-                result[cid.id] = int(t['hold']), int(t['tower'])
+                result[cid.id] = int(t['hold'] or 0), int(t['tower'] or 0)
     return result
