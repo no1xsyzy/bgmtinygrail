949d64f1cfc91c280decc806a0490b629e750dbb
no1xsyzy <mf20070535@126.com>
2020-10-01 01:31:51 +0800

    clean(trader.gracefully): cleanup

--- trader/gracefully.py
+++ trader/gracefully.py
@@ -1,5 +1,3 @@
-from typing import *
-
 from tinygrail.api import scratch_bonus2, scratch_gensokyo
 from .fundamentally import *
 
@@ -10,36 +8,6 @@ scratch_translate = {
 
 
 class GracefulTrader(FundamentalTrader):
-    fixed: List[int]
-
-    def __init__(self, player):
-        super().__init__(player)
-        self.init_fixed()
-
-    def init_fixed(self):
-        self.fixed = []
-
-    def get_bonus(self, rule: Optional[Tuple[str, Union[Literal['inf'], int]]] = None):
-        if rule is None:
-            rule = ('bonus2', 'inf'),
-        results = {}
-        for scratch_type, times in rule:
-            if isinstance(times, int):
-                iterable = range(times)
-            else:
-                assert times == 'inf'
-                import itertools
-                iterable = itertools.count()
-            for t in iterable:
-                scratch_result = scratch_translate[scratch_type](self.player)
-                if scratch_result is None:
-                    break
-                for sb in scratch_result:
-                    logger.debug(f"scratch_{scratch_type:<10} | "
-                                 f"got #{sb.id:<5} | {sb.amount=}, {sb.sell_price=}")
-                    results[sb.id] = sb.sell_price
-        return list(results.items())
-
     def graceful_tick(self, cid, sell_price) -> float:
         """indicated for daily bonus"""
         big_c = self.big_c(cid)
