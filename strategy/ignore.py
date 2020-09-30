921f2df9f249a7e57a9e5e97b3e7783b58a6b810
no1xsyzy <mf20070535@126.com>
2020-09-30 23:35:10 +0800

    enhance(strategy.ignore): forced_view

--- strategy/ignore.py
+++ strategy/ignore.py
@@ -7,11 +7,14 @@ class IgnoreStrategy(ABCCharaStrategy):
     def transition(self):
         from strategy import BalanceStrategy
         if self.big_c.total_holding > 0:
-            if self.big_c.amount > 0:
-                logger.info("new stock")
-                self._fast_seller(low=self._exchange_price)
-                if self.big_c.amount or self.big_c.asks:
-                    return self._transact(BalanceStrategy)
+            logger.info("new stock")
+            self._fast_seller(low=self._exchange_price)
+            if self.big_c.amount or self.big_c.asks:
+                return self._transact(BalanceStrategy)
+        elif self.big_c.bids[0].price == 2.0 and self.big_c.bids[0].amount == 2:
+            logger.info("forced view")
+            self._fast_forward(self._exchange_price)
+            self._output_balanced()
         return self
 
     def output(self):
