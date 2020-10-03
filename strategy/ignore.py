c751f1fc70457f0a0406f77690ff269df454b0d3
no1xsyzy <mf20070535@126.com>
2020-10-04 04:51:04 +0800

    fix(strategy.ignore): error for force view testing
    if neither holding nor bidding, causes out of range

--- strategy/ignore.py
+++ strategy/ignore.py
@@ -15,7 +15,7 @@ class IgnoreStrategy(ABCCharaStrategy):
             self._fast_seller(low=self._exchange_price)
             if self.big_c.amount or self.big_c.asks:
                 return self._transact(BalanceStrategy)
-        elif self.big_c.bids[0].price == 2.0 and self.big_c.bids[0].amount == 2:
+        elif self.big_c.bids and self.big_c.bids[0].price == 2.0 and self.big_c.bids[0].amount == 2:
             logger.info("forced view")
             self._fast_forward(self._exchange_price)
             self._output_balanced()
