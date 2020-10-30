e2334d94e4893b6c288fe85db46d02597b3d2948
no1xsyzy <mf20070535@126.com>
2020-10-31 00:20:00 +0800

    fix(spoil_holders): RMinimalUserCharacter can return TAuction
    And TAuction is now showed last

--- src/bgmtinygrail/cli/spoil_holders.py
+++ src/bgmtinygrail/cli/spoil_holders.py
@@ -2,6 +2,7 @@ import click
 
 from ._base import TG_PLAYER
 from ..tinygrail.api import spoil_holders as api_spoil_holders
+from ..tinygrail.model import TMinimalUserCharacter, TAuction
 from ..tinygrail.player import Player
 
 
@@ -10,5 +11,11 @@ from ..tinygrail.player import Player
 @click.argument('cid', type=int)
 def spoil_holders(player: Player, cid: int):
     holders = api_spoil_holders(player, cid)
+    auction = None
     for muc, holder in holders:
-        click.echo(f"{holder.nickname}({holder.name}) - {muc.total}/{muc.sacrifices}")
+        if isinstance(muc, TMinimalUserCharacter):
+            click.echo(f"{holder.nickname}({holder.name}) - {muc.total}/{muc.sacrifices}")
+        elif isinstance(muc, TAuction):
+            auction = muc
+    if auction is not None:
+        click.echo(f"Totally {auction.total}, {auction.auction_users} user(s) raced for {auction.auction_total} stocks")
