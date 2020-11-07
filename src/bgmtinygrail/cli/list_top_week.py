47f7b8281dd3291bfcb156e43a91774e93e12e22
no1xsyzy <mf20070535@126.com>
2020-11-07 12:53:14 +0800

    progress(cli.list_top_week): better csv style

--- src/bgmtinygrail/cli/list_top_week.py
+++ src/bgmtinygrail/cli/list_top_week.py
@@ -5,7 +5,23 @@ from ..tinygrail import top_week
 
 @click.command()
 def list_top_week():
-    print("CID,name,price,extra,total_users,total_request,in_valhalla")
-    for auction in top_week():
-        print(f"#{auction.character_id},{auction.character_name},{auction.price},{auction.extra},"
-              f"{auction.type},{auction.assets},{auction.sacrifices}")
+    top = top_week()
+    from csv import DictWriter
+    import sys
+    writer = DictWriter(sys.stdout, fieldnames=['rank', 'CID', 'name', 'price', 'extra',
+                                                'total_users', 'total_request', 'in_valhalla',
+                                                'score_1', 'score_2'])
+    writer.writeheader()
+    for idx, auction in enumerate(top):
+        writer.writerow({
+            'rank': idx + 1,
+            'CID': f'#{auction.character_id}',
+            'name': auction.character_name,
+            'price': auction.price,
+            'extra': auction.extra,
+            'total_users': auction.type,
+            'total_request': auction.assets,
+            'in_valhalla': auction.sacrifices,
+            'score_1': auction.score_1,
+            'score_2': auction.score_2,
+        })
