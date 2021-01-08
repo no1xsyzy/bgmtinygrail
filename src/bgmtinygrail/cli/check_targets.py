d3d42fcccb3c381b7d18bb39445dc5f462472038
no1xsyzy <mf20070535@126.com>
2021-01-08 21:31:43 +0800

    progress(cli.check_targets): split in-markets and ICOs
    more information for ICOs

--- src/bgmtinygrail/cli/check_targets.py
+++ src/bgmtinygrail/cli/check_targets.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 import click
@@ -5,7 +6,9 @@ from click.utils import LazyFile
 
 from ._base import TG_PLAYER
 from ._helpers import parse_target
-from ..tinygrail.api import batch_character_info, get_full_holding_2
+from ..tinygrail import ServerSentError, ico_minimal_investment_for_level, ico_now_level_by_investment, \
+    ico_now_level_by_investors, ico_minimal_investors_for_level, ico_offerings_for_level
+from ..tinygrail.api import batch_character_info, get_full_holding_2, my_initial_for_character
 from ..tinygrail.model import TICO, TTemple
 
 
@@ -32,7 +35,9 @@ def check_targets(player, targets: List[str], from_file: List[LazyFile], show_ex
         raise click.exceptions.Exit(11)
     characters = get_full_holding_2(player)
 
-    table = {}
+    initialized = {}
+    in_initial = []
+
     checks = []
     for cid, (th, tt) in parsed_targets.items():
         if character := characters.get(cid):
@@ -50,9 +55,9 @@ def check_targets(player, targets: List[str], from_file: List[LazyFile], show_ex
                     else:
                         return f"{actual}"
 
-                table[cid] = [f"#{cid}", character.name, f"{th}/{tt}",
-                              f"\033[32m{ch}/{ct}\033[0m" if ch + ct >= th + tt and ct < tt else
-                              f"{colored(ch, th)}/{colored(ct, tt)}"]
+                initialized[cid] = [f"#{cid}", character.name, f"{th}/{tt}",
+                                    f"\033[32m{ch}/{ct}\033[0m" if ch + ct >= th + tt and ct < tt else
+                                    f"{colored(ch, th)}/{colored(ct, tt)}"]
         else:
             if (th, tt) != (0, 0):
                 checks.append(cid)
@@ -62,11 +67,63 @@ def check_targets(player, targets: List[str], from_file: List[LazyFile], show_ex
             cid = c.character_id
             th, tt = parsed_targets[cid]
             if isinstance(c, TICO):
-                table[cid] = [f"#{cid}", c.name, f"{th}/{tt}", f"ICO: {c.total}"]
+                try:
+                    my_initial = my_initial_for_character(player, c.id)
+                    my_investment = my_initial.amount
+                except ServerSentError as e:
+                    if e.message != '尚未参加ICO。':
+                        raise
+                    my_investment = 0
+                total_investment = c.total
+                total_investors = c.users
+                end_date = c.end.replace(tzinfo=None)
+                lo_lv, up_lv = sorted((ico_now_level_by_investment(total_investment),
+                                       ico_now_level_by_investors(total_investors)))
+                if lo_lv < 1:
+                    lo_lv = 1
+                for level in range(lo_lv, up_lv + 2):
+                    min_investment = ico_minimal_investment_for_level(level)
+                    min_investors = ico_minimal_investors_for_level(level)
+                    offerings = ico_offerings_for_level(level)
+                    more_investment = min_investment - total_investment
+                    more_investors = min_investors - total_investors
+                    stocks_for_me = th + tt
+                    stocks_for_others = offerings - stocks_for_me
+                    investment_others_part = total_investment - my_investment + more_investors * 5000
+                    investment_my_part = max(
+                        math.ceil(investment_others_part / stocks_for_others * stocks_for_me),
+                        ico_minimal_investment_for_level(level) / ico_offerings_for_level(level) * stocks_for_me)
+                    more_investment_my_part = investment_my_part - my_investment
+                    in_initial.append([
+                        (end_date, 1),
+                        [f"#{cid}", c.name, f"{end_date}", f"{th}/{tt}({th + tt})",
+                         f"{level}", f"{offerings}", f"{my_investment}", f"{total_investment}", f"{total_investors}",
+                         (f"{more_investment}" if more_investment > 0 else "(met)"),
+                         (f"{more_investors}" if more_investors > 0 else "(met)"),
+                         f"{investment_my_part}",
+                         (f"{more_investment_my_part}" if more_investment_my_part > 0 else "(met)"),
+                         ]])
             else:
-                table[cid] = [f"#{cid}", c.name, f"{th}/{tt}", "-"]
+                initialized[cid] = [f"#{cid}", c.name, f"{th}/{tt}", "-"]
             checks.remove(cid)
 
     from tabulate import tabulate
-    click.echo(tabulate([table[key] for key in sorted(table.keys())], ('CID', 'name', 'Target', 'Actual'),
-                        output_format))
+    if initialized:
+        click.echo("In market:")
+        click.echo(tabulate([initialized[key] for key in sorted(initialized.keys())],
+                            ('CID', 'Name', 'Target', 'Actual'),
+                            output_format))
+
+    if in_initial and initialized:
+        click.echo()
+
+    if in_initial:
+        click.echo("ICOs:")
+        click.echo(tabulate([x[1] for x in sorted(in_initial, key=lambda x: x[0])],
+                            ('CID', 'Name', 'End Date', 'Target',
+                             'Lv', '总发行', '自投入₵', '总投入₵', '人数',
+                             '还需₵', '还需人数', '目标投入₵', '还需投入₵'),
+                            output_format))
+
+    if not in_initial and not initialized:
+        click.echo("Nothing to show")
