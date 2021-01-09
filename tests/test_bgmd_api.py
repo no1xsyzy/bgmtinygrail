a0db86426bfad65ffe9dfd90e57b9209f1155826
no1xsyzy <mf20070535@126.com>
2021-01-09 18:57:02 +0800

    perf(tests.test_bgmd_api): Use tinygrail player for better performance

--- tests/test_bgmd_api.py
+++ tests/test_bgmd_api.py
@@ -4,37 +4,39 @@ from pytest_mock import MockerFixture
 from bgmtinygrail.bgmd.api import *
 from bgmtinygrail.bgmd.model import *
 
-user = user_info(285822)
+user = user_info(525688)
 
 
 class TestUserInfo:
     def test_user_info(self):
-        assert user.id == 285822
-        assert user.url == 'http://bgm.tv/user/rog_defwang'
-        assert user.username == 'rog_defwang'
-        assert user.nickname == '凡付真遠'
+        assert user.id == 525688
+        assert user.url == "http://bgm.tv/user/525688"
+        assert user.username == '525688'
+        assert user.nickname == "小圣杯玩家"
 
 
 class TestUserMono:
     def test_character(self):
         result = user_mono(user, 'character')
-        assert len(result) == 1591
-        assert isinstance(result[0], Character) and result[0].id == 76074
-        assert isinstance(result[-1], Character) and result[-1].id == 36489
+        assert len(result) == 64
+        assert isinstance(result[0], Character) and result[0].id == 65
+        assert isinstance(result[-1], Character) and result[-1].id == 1
+        assert 56 not in {c.id for c in result}  # Character(id=56) doesn't exists
 
     def test_person(self):
         result = user_mono(user, 'person')
-        assert len(result) == 298
-        assert isinstance(result[0], Person) and result[0].id == 25428
-        assert isinstance(result[-1], Person) and result[-1].id == 12507
+        assert len(result) == 65
+        assert isinstance(result[0], Person) and result[0].id == 65
+        assert isinstance(result[-1], Person) and result[-1].id == 1
 
     def test_both(self):
         result = user_mono(user, 'both')
-        assert len(result) == 1889
-        assert isinstance(result[0], Character) and result[0].id == 76074
-        assert isinstance(result[1590], Character) and result[1590].id == 36489
-        assert isinstance(result[1591], Person) and result[1591].id == 25428
-        assert isinstance(result[-1], Person) and result[-1].id == 12507
+        assert len(result) == 129
+        assert isinstance(result[0], Character) and result[0].id == 65
+        assert isinstance(result[63], Character) and result[63].id == 1
+        assert isinstance(result[64], Person) and result[64].id == 65
+        assert isinstance(result[-1], Person) and result[-1].id == 1
+        assert (Character, 56) not in {(type(c), c.id) for c in result}
 
     def test_wrong(self):
         with pytest.raises(AssertionError) as exc_info:
